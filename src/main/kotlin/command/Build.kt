aefe065c713f4a69db5b427811eddc5455761465
turtton <top.gear7509@turtton.net>
2023-01-24 19:17:04 +0900

    feat: replace build script

--- src/main/kotlin/command/Build.kt
+++ src/main/kotlin/command/Build.kt
@@ -1,35 +1,44 @@
 package command
 
 import create
+import execMessage
 import io.CONTENT_DIR
 import io.VOLGLASS_DIR
-import io.atomicMove
-import io.copy
-import io.list
+import io.copyRecursively
 import io.mkdir
+import io.pnpmVolglass
+import io.spawnAsync
 import log
 import okio.Path.Companion.toPath
 
 class Build : Prepare() {
 
     override suspend fun runAsChild() {
+        val export = pnpmVolglass("run", "export")
+        echo(execMessage(export))
+        spawnAsync(export)
+
         echo(create(CONTENT_DIR))
         val contentDir = CONTENT_DIR.toPath()
         contentDir.mkdir()
+
         val volglassDir = VOLGLASS_DIR.toPath()
-        val nextConfig = "next.config.js"
-        volglassDir.div(nextConfig).copy(contentDir.div(nextConfig))
-        val publicDir = "public"
-        volglassDir.div(publicDir).atomicMove(contentDir.div(publicDir))
-        val packageJson = "package.json"
-        volglassDir.div(packageJson).copy(contentDir.div(packageJson))
-        val nextDir = ".next"
-        val nextDirPath = volglassDir.div(nextDir)
-        nextDirPath.div("standalone").list().getOrNull()?.forEach {
-            it.atomicMove(contentDir)
-        }
-        val staticDir = "static"
-        nextDirPath.div(staticDir).atomicMove(contentDir.div(staticDir))
+
+        volglassDir.div("out").copyRecursively(contentDir)
+
+        // val nextConfig = "next.config.js"
+        // volglassDir.div(nextConfig).copy(contentDir.div(nextConfig))
+        // val publicDir = "public"
+        // volglassDir.div(publicDir).copyRecursively(contentDir.div(publicDir))
+        // val packageJson = "package.json"
+        // volglassDir.div(packageJson).copy(contentDir.div(packageJson))
+        // val nextDir = ".next"
+        // val nextDirPath = volglassDir.div(nextDir)
+        // nextDirPath.div("standalone").copyRecursively(contentDir)
+        // val staticDir = "static"
+        // val contentNextJsDir = contentDir.div(nextDir)
+        // contentNextJsDir.mkdir()
+        // nextDirPath.div(staticDir).copyRecursively(contentNextJsDir.div(staticDir))
         echo(log("Done", "Content generated in $CONTENT_DIR"))
     }
 }
