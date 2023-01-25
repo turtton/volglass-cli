a615a66637db0d30eef92566c82e68c092a11681
turtton <top.gear7509@turtton.net>
2023-01-25 12:47:36 +0900

    feat: add info log pattern

--- src/main/kotlin/Log.kt
+++ src/main/kotlin/Log.kt
@@ -8,4 +8,6 @@ fun note(message: String): String = log("Note", message)
 
 fun warn(message: String): String = log("Warn", message)
 
+fun info(message: String): String = log("Info", message)
+
 fun log(prefix: String, message: String): String = "[$prefix] $message"
